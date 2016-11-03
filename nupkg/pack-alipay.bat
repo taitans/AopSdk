aebb87676607312f6d020e282a26a2268c743561
Loongle <x120039483@hotmail.com>
2016-11-03 17:51:07 +0800

    Update publish cmd

--- nupkg/pack-alipay.bat
+++ nupkg/pack-alipay.bat
@@ -1,1 +1,2 @@
 "..\.nuget\NuGet.exe" "pack" "..\src\Taitans.AopSdk\Taitans.AopSdk.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Build
+"..\.nuget\NuGet.exe" "pack" "..\src\Taitans.AopSdk.Core\Taitans.AopSdk.Core.csproj" -Properties Configuration=Release -IncludeReferencedProjects -Build
